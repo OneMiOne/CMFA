e1ca0737a83ea4976f86fdb5abd4fa5a97269756
FengWuShu <106773566+OneMiOne@users.noreply.github.com>
2026-02-06 07:37:16 +0800

    Feng

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ task("downloadGeoFiles") {
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat" to "geoip.dat",
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat" to "geosite.dat",
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb" to "country.mmdb",
-        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb" to "asn.mmdb",
+        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb" to "ASN.mmdb",
     )
 
     doLast {
