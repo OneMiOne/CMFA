a79ddc60310493eb4dab14b2bb44ee7d9ca4c89c
FengWuShu <106773566+OneMiOne@users.noreply.github.com>
2026-02-06 06:35:06 +0800

    Feng

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,10 @@ task("downloadGeoFiles") {
 
     val geoFilesUrls = mapOf(
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.metadb" to "geoip.metadb",
+        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat" to "geoip.dat",
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat" to "geosite.dat",
-        // "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb" to "country.mmdb",
-        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb" to "ASN.mmdb",
+        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb" to "country.mmdb",
+        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb" to "asn.mmdb",
     )
 
     doLast {
